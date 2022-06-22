66a7b64bfd8e0c793bef6648be95830301312146
DamianoCeka <damiano.ceka83@gmail.com>
2022-06-22 14:23:54 +0200

    Aggiungere i file di progetto.

---
+++ AbstractClass.Damiano/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AbstractClass.Damiano
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
